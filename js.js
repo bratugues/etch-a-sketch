c0f45dad3bbe456bf93d9dbbfe676564d6cd63eb
Erick <erickloboabreu@hotmail.com>
2025-10-03 23:11:11 -0300

    add hover to change square background

--- js.js
+++ js.js
@@ -2,20 +2,34 @@ const container = document.querySelector(".container");
 const resizeBtn = document.querySelector(".resize")
 let numPerSide = 16;
 
+const addHoverListeners = () => {
+  const squares = document.querySelectorAll(".square")
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.background = "black"
+    })
+  })
+}
+
 const createDivs = (numPerSide) => {
   container.innerHTML = "";
   for (let i = 0; i < (numPerSide * numPerSide); i++) {
     const div = document.createElement("div");
     div.classList.add("square")
-    const squareSize = 500 / numPerSide;
-    div.style.width = squareSize + "px";
-    div.style.height = squareSize + "px";
+    const squareSize = 600 / numPerSide;
+    div.style.width = `${squareSize}px`;
+    div.style.height = `${squareSize}px`;
     container.appendChild(div)
   }
 }
+
 createDivs(numPerSide)
+
 
 resizeBtn.addEventListener("click", () =>{
   numPerSide = prompt("Select size between 1-100:")
   createDivs(Number(numPerSide));
+  addHoverListeners()
 })
+
+addHoverListeners()
